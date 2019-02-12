7f9a760fde06429dc5e65cbfbe37c74928da69a2
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-12 22:28:13 +0000

    add default value to user, key press event on message and names

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
-  /* needs a constructor */
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" name="username" defaultValue={this.props.currentUser} placeholder="Your Name (Optional)" />
+        <input onKeyPress={this.props.onKeyPress} className="chatbar-message" name="message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
