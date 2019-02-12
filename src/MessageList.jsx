9c4d2ddf29f84190ff4a49b5f6805f8d78f0abe9
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-12 15:41:27 +0000

    create MessageList component

---
+++ src/MessageList.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+    /* if () {} */
+
+    /* else */
+        <main className="messages">
+          <Message />
+        </main>
+    );
+  }
+
+}
+
+export default MessageList;
