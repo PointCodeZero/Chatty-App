694bb1a4b4adeec47692a2c852c4edfce3b63ad2
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-13 23:16:10 +0000

    add uuid and imprlemnted broadcast to all users

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
+const uuidv4 = require('uuid/v4');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -16,12 +18,25 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('message', data => {
-    const JSONData = JSON.parse(data);
-    console.log(`User ${JSONData.username} said ${JSONData.content}`);
+    const message = JSON.parse(data);
+    const uniqueID = uuidv4();
+    const newObj = { id: uniqueID, ...message };
+    wss.broadcast(JSON.stringify(newObj))
   });
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
