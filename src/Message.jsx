56f299341d98cf8dcc622ca9ec026ff89bfaf447
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-13 23:13:14 +0000

    removed key id from dic tag

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,7 +4,7 @@ class Message extends Component {
   render() {
     return (
       <div>
-        <div key={this.props.message.id} className="message">
+        <div className="message">
           <span className="message-username">{ this.props.message.username }</span>
           <span className="message-content">{ this.props.message.content }</span>
         </div>
