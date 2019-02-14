498667d0bdfd16372156afa03b846d09dc21b875
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-14 17:57:29 +0000

    add type to state, send and receive type message, changed name of onKeyPress method to addMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
@@ -6,27 +6,36 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      type: '',
       currentUser: 'Anonymous',
       messages: []
     };
-    this.onKeyPress = this.onKeyPress.bind(this);
+    this.addMessage = this.addMessage.bind(this);
     this.currentUserUpdate = this.currentUserUpdate.bind(this);
   }
 
-  onKeyPress(event) {
+  addMessage(event) {
     if (event.key == 'Enter') {
-      this.socket.send(JSON.stringify({ username: this.state.currentUser, content: event.target.value }));
+      this.socket.send(
+        JSON.stringify({
+          type: 'postMessages',
+          username: this.state.currentUser,
+          content: event.target.value
+        })
+      );
       event.target.value = '';
     }
   }
 
   currentUserUpdate(event) {
     const newUser = event.target.value;
-    newUser.length === 0 ? this.setState({ currentUser: 'Anonymous' }) : this.setState({ currentUser: newUser });
+    newUser.length === 0
+      ? this.setState({ currentUser: 'Anonymous' })
+      : this.setState({ currentUser: newUser });
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
+    console.log('componentDidMount <App />');
 
     //Websockets connection
     this.socket = new WebSocket('ws://localhost:3001');
@@ -34,21 +43,47 @@ class App extends Component {
       console.log('Connected to 3001');
     };
 
-    this.socket.onmessage = (event) => {
+    this.socket.onmessage = event => {
       const messages = this.state.messages;
-      this.setState({ messages: [...messages, JSON.parse(event.data)] }, () => {
-      });
-    }
+      const data = JSON.parse(event.data);
+      switch (data.type) {
+        case 'incomingMessage':
+          this.setState(
+            {
+              messages: [
+                ...messages,
+                { id: data.id, username: data.username, content: data.content }
+              ]
+            },
+            () => {}
+          );
+          break;
+        // case 'incomingNotification':
+        //   // this.setState({ messages: [...messages, data] }, () => { });
+        //   break;
+        default:
+          throw new Error(`Unknown event type ${data.type}`);
+      }
+    };
   }
 
   render() {
     return (
       <div>
         <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
+          <a href="/" className="navbar-brand">
+            Chatty
+          </a>
         </nav>
-        <MessageList messages={this.state.messages} currentUser={this.state.currentUser} />
-        <ChatBar onKeyPress={this.onKeyPress} currentUser={this.state.currentUser} currentUserUpdate={this.currentUserUpdate} />
+        <MessageList
+          messages={this.state.messages}
+          currentUser={this.state.currentUser}
+        />
+        <ChatBar
+          addMessage={this.addMessage}
+          currentUser={this.state.currentUser}
+          currentUserUpdate={this.currentUserUpdate}
+        />
       </div>
     );
   }
