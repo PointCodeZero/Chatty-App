e6530ad54383a19ce2477c30cbd28e9d5c613448
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-15 19:04:01 +0000

    add time stamp

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
     return (
       <div>
         <div className="message">
-          <span className="message-username">{ this.props.message.username }</span>
-          <span className="message-content">{ this.props.message.content }</span>
+          <span className="message-username">
+            {this.props.message.username}
+          </span>
+          <span className="message-content">{this.props.message.content}</span>
+          <span className="message-content">
+            {this.props.message.created_at}
+          </span>
         </div>
       </div>
     );
