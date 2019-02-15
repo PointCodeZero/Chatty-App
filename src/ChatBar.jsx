0d66e4f6d28d779ea93bbf823990878a4638e0e6
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-15 01:59:33 +0000

    passed props to input username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,7 @@ class ChatBar extends Component {
   render() {
     return (
       <footer className="chatbar">
-        <input onChange={this.props.currentUserUpdate} className="chatbar-username" type="text" defaultValue={this.props.currentUser} placeholder="Your Name (Optional)" />
+        <input onKeyPress={this.props.addNotification} onChange={this.props.currentUserUpdate} className="chatbar-username" type="text" defaultValue={this.props.currentUser} placeholder="Your Name (Optional)" />
         <input onKeyPress={this.props.addMessage} className="chatbar-message" type="text" placeholder="Type a message and hit ENTER" />
       </footer>
     );
