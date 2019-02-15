8c01d47707c9f1b0f2c6ea673d3b70d6fd3f970c
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-15 19:02:06 +0000

    add time stamp and removed unnecessary comments and logs

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,26 +2,19 @@ const express = require('express');
 const WebSocket = require('ws');
 const SocketServer = WebSocket.Server;
 const uuidv4 = require('uuid/v4');
-
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
+// Express server
 const server = express()
-  // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () =>
     console.log(`Listening on ${PORT}`)
   );
 
-// Create the WebSockets server
+// WebSockets server
 const wss = new SocketServer({ server });
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
-
-//Send all updates to all users
+// Send all updates to all users
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
@@ -45,7 +38,6 @@ wss.on('connection', ws => {
   //Broadcast messages and notifications
   ws.on('message', handleMessage);
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
 
