0df2c8e457caf2518e4db238f86e5c6883d4250e
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-15 02:00:20 +0000

    add if statement to decide if notification or message will be posted

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import Notification from './Notification.jsx';
 
 class MessageList extends Component {
   render() {
     return (
-      <main className="messages">{this.props.messages.map((message) => {
-        return <Message key={message.id} message={message}/>
+      <main className="messages">
+        {this.props.messagesNotifications.map((item) => {
+          if(item.type === 'incomingMessage') {
+            return <Message key={item.id} message={item} />
+          }
+          return <Notification key={item.id} notification={item} />
         })}
       </main>
     );
   }
-
 }
 
 export default MessageList;
